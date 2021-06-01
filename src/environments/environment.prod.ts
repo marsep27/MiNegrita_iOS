397e6061eff0c83aa0d0dfb322f5f02b3b367703
Marco Soto Morera <marsep27@hotmai.com>
2021-06-01 00:47:43 -0600

    Se actualiza la conexión con la base de datos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,6 +11,7 @@ export const firebaseConfig = {
     projectId: "db-m1n3gr1t4",
     storageBucket: "db-m1n3gr1t4.appspot.com",
     messagingSenderId: "674389939864",
-    appId: "1:674389939864:web:0402cba07a7ebbee9e22ed"
+    appId: "1:674389939864:web:0402cba07a7ebbee9e22ed",
+    measurementId: "G-FHRKT6E1CR"
   }
 };
